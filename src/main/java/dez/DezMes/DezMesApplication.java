ca2b7ae7b030ed8fe303837b5f7d09aefb64bce1
denis <deniskabutkevich@gmail.com>
2020-07-17 00:42:27 +0300

    start project

---
+++ src/main/java/dez/DezMes/DezMesApplication.java
@@ -0,0 +1,13 @@
+package dez.DezMes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DezMesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DezMesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dez/DezMes/DezMesApplicationTests.java
@@ -0,0 +1,13 @@
+package dez.DezMes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DezMesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
