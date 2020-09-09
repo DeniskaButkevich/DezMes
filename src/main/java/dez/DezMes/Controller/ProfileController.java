898711dc1c0e8891accc55b2f88bdb0eb6a06350
Denis Butkevich <deniskabutkevich@gmail.com>
2020-09-09 14:21:01 +0300

    Confirmed subscriptions: frontend

--- src/main/java/dez/DezMes/Controller/ProfileController.java
+++ src/main/java/dez/DezMes/Controller/ProfileController.java
@@ -2,6 +2,7 @@ package dez.DezMes.Controller;
 
 import com.fasterxml.jackson.annotation.JsonView;
 import dez.DezMes.domain.User;
+import dez.DezMes.domain.UserSubscription;
 import dez.DezMes.domain.Views;
 import dez.DezMes.repo.UserDetailsRepo;
 import dez.DezMes.service.ProfileService;
@@ -9,19 +10,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("profile")
 public class ProfileController {
     private final ProfileService profileService;
+    private final UserDetailsRepo userDetailsRepo;
 
     @Autowired
-    public ProfileController(ProfileService profileService) {
+    public ProfileController(ProfileService profileService, UserDetailsRepo userDetailsRepo) {
         this.profileService = profileService;
+        this.userDetailsRepo = userDetailsRepo;
     }
 
     @GetMapping("{id}")
     @JsonView(Views.FullProfile.class)
-    public User get(@PathVariable("id") User user) {
+    public User get(@PathVariable("id") String id) {
+        User user = userDetailsRepo.findById(id).get();
         return user;
     }
 
@@ -29,12 +35,32 @@ public class ProfileController {
     @JsonView(Views.FullProfile.class)
     public User changeSubscription(
             @AuthenticationPrincipal User subscriber,
-            @PathVariable("channelId") User channel
+            @PathVariable("channelId") String id
     ) {
+        User channel = userDetailsRepo.findById(id).get();
         if (subscriber.equals(channel)) {
             return channel;
         } else {
             return profileService.changeSubscription(channel, subscriber);
         }
+    }
+
+    @GetMapping("get-subscribers/{channelId}")
+    @JsonView(Views.IdName.class)
+    public List<UserSubscription> subscribers(
+            @PathVariable("channelId") String channel_id
+    ) {
+        User channel = userDetailsRepo.findById(channel_id).get();
+        return profileService.getSubscribers(channel);
+    }
+
+    @PostMapping("change-status/{subscriberId}")
+    @JsonView(Views.IdName.class)
+    public UserSubscription changeSubscriptionStatus(
+            @AuthenticationPrincipal User channel,
+            @PathVariable("subscriberId") String subscriber_id
+    ) {
+        User subscriber = userDetailsRepo.findById(subscriber_id).get();
+        return profileService.changeSubscriptionStatus(channel, subscriber);
     }
 }
